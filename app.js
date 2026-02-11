47b7075088994562be5b5561ae49e152842fd5e6
zhiaam <zhiabanjarmasin55@gmail.com>
2026-02-11 20:45:38 +0700

    feat: add task creation functionality

---
+++ app.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("task-form");
+const input = document.getElementById("task-input");
+const list = document.getElementById("task-list");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  addTask(input.value);
+  input.value = "";
+});
+
+function addTask(text) {
+  const li = document.createElement("li");
+  li.innerHTML = `${text} <button>X</button>`;
+  list.appendChild(li);
+}
